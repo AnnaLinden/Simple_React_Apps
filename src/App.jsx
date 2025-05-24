f272e5db74ee70cb3bf8fd177887def92a980a99
Anna Linden <linden.a.anna@gmail.com>
2025-05-24 18:01:16 +0300

    carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import ToggleTheme from "./components/5.ColorToggler/ToggleTheme";
+import ReactInterview from "./components/7.ReactInterview/ReactInterview";
 
 function App() {
-  return <>{/*  <ToggleTheme />*/}</>;
+  return (
+    <>
+      {/*  <ToggleTheme />*/}
+      <ReactInterview />
+    </>
+  );
 }
 
 export default App;
